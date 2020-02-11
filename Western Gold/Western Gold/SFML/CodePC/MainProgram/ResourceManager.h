4f72e4d4642e08fefd8e47f77fe263ed71072e9d
SimonStrands <36299106+SimonStrands@users.noreply.github.com>
2020-02-11 09:27:55 +0100

    resourceManager.h

--- Western Gold/Western Gold/SFML/CodePC/MainProgram/ResourceManager.h
+++ Western Gold/Western Gold/SFML/CodePC/MainProgram/ResourceManager.h
@@ -8,12 +8,11 @@ public:
 	ResourceManager();
 	~ResourceManager();
 
-	void windowSetup(int width, int height);
+	void windowSetup(sf::RenderWindow* window);
 	int getWindowWidth() const;
 	int getWindowHeight() const;
+	sf::RenderWindow* getWindow();
 
-	
-	
 #pragma region Fonts
 	sf::Font* getBasicFont();
 #pragma endregion
@@ -27,17 +26,16 @@ public:
 	sf::Image* getLevel_Test();
 #pragma endregion
 
-
-
 	//debug
-	
+
 	sf::SoundBuffer* getass();
 	sf::Texture* gettex();
 
 private:
 	int windowWidth;
 	int windowHeight;
-	
+	sf::RenderWindow* window;
+
 #pragma region Fonts
 	sf::Font* basicFont;
 #pragma endregion
@@ -49,6 +47,10 @@ private:
 
 #pragma region Levels
 	sf::Image* level_test;
+#pragma endregion
+
+#pragma region Sounds
+
 #pragma endregion
 
 	//debug
