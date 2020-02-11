4c7d32a18b3fd9a85241554a17abd8e2193f6b7a
Simon Strand <etta11@hotmail.se>
2020-02-11 23:55:03 +0100

    collision Left and Right

--- Western Gold/Western Gold/SFML/CodePC/MainProgram/Collision.cpp
+++ Western Gold/Western Gold/SFML/CodePC/MainProgram/Collision.cpp
@@ -5,40 +5,82 @@ void Collision::checkCollision()
 {
 	if (tiles->getIsWalkable()) {
 		while (tiles->getSprite()->getGlobalBounds().intersects(gameObjects->getBounds())) {
-			if (tiles->getSprite()->getGlobalBounds().left < gameObjects->getBounds().left) {
 				if (rightSide(gameObjects, tiles)) {
-
+					std::cout << "Right" << std::endl;
+					gameObjects->moveSprite(1, 0);
 				}
 				else if (leftSide(gameObjects, tiles)) {
-
+					std::cout << "Left" << std::endl;
+					gameObjects->moveSprite(-1, 0);
 				}
 				else if (topSide(gameObjects, tiles)) {
-
+					gameObjects->moveSprite(0, -1);
 				}
 				else if (botSide(gameObjects, tiles)) {
-
+					gameObjects->moveSprite(0, 1);
 				}
-			}
+				else {
+					//MessageBox(nullptr, L"Error With Collision", L"ERROR", MB_ICONWARNING | MB_OK);
+				}
+			
 		}
 	}
 }
 
-bool Collision::rightSide(GameObject* player, tile* tiles)
+bool Collision::rightSide(GameObject* gameObject, tile* tiles)
 {
-	return ;
+	bool theReturn = false;
+	sf::FloatRect cTile = tiles->getSprite()->getGlobalBounds();
+	if (gameObject->getRight() > (cTile.left + cTile.width)) {
+		if (gameObject->getTop() > cTile.top && gameObject->getBot() < (cTile.top + cTile.height)) {
+			theReturn = true;
+		}
+		else if (gameObject->getTop() < cTile.top && gameObject->getBot() > (cTile.top + cTile.height)) {
+			theReturn = true;
+		}
+		else if (gameObject->getTop() < cTile.top) {
+			if (cTile.top - gameObject->getTop() < gameObject->getRight() - (cTile.left + cTile.width)) {
+				theReturn = true;
+			}
+		}
+		else {
+			if (gameObject->getBot() - (cTile.top + cTile.height) < gameObject->getRight() - (cTile.left + cTile.width)) {
+				theReturn = true;
+			}
+		}
+	}
+	return theReturn;
 }
 
-bool Collision::leftSide(GameObject* player, tile* tiles)
+bool Collision::leftSide(GameObject* gameObject, tile* tiles)
+{
+
+	bool theReturn = false;
+	sf::FloatRect cTile = tiles->getSprite()->getGlobalBounds();
+	if (gameObject->getLeft() < cTile.left) {
+		if (gameObject->getTop() > cTile.top && gameObject->getBot() < (cTile.top + cTile.height)){
+			theReturn = true;
+		}
+		if(gameObject->getTop() < cTile.top && gameObject->getBot() > (cTile.top + cTile.height)){
+			theReturn = true;
+		}
+		if (gameObject->getTop() < cTile.top && cTile.top - gameObject->getTop() < cTile.left - gameObject->getLeft()) {
+				theReturn = true;
+		}
+		if (gameObject->getBot() - (cTile.top + cTile.height) < cTile.left - gameObject->getLeft()) {
+				theReturn = true;
+		}
+			
+	}
+	return theReturn;
+}
+
+bool Collision::topSide(GameObject* gameObject, tile* tiles)
 {
 	return false;
 }
 
-bool Collision::topSide(GameObject* player, tile* tiles)
-{
-	return false;
-}
-
-bool Collision::botSide(GameObject* player, tile* tiles)
+bool Collision::botSide(GameObject* gameObject, tile* tiles)
 {
 	return false;
 }
