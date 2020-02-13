f334e660c7a97ddb4f74fb9b658469dfbb2b49a0
SimonStrands <36299106+SimonStrands@users.noreply.github.com>
2020-02-13 10:18:48 +0100

    någon collision update

--- Western Gold/Western Gold/SFML/CodePC/MainProgram/Collision.h
+++ Western Gold/Western Gold/SFML/CodePC/MainProgram/Collision.h
@@ -3,16 +3,18 @@
 #include "Player.h"
 //#include "Gold"
 #include "Ray.h"
+#include "GetDistance.h"
 
 class Collision {
 private:
 	GameObject* gameObjects;
 	tile* tiles;
 	void checkCollision();
-	bool rightSide(GameObject* player, tile* tiles);
-	bool leftSide(GameObject* player, tile* tiles);
-	bool topSide(GameObject* player, tile* tiles);
-	bool botSide(GameObject* player, tile* tiles);
+	//if gameobject is on the right side of tiles
+	bool rightSide(GameObject*	gameObject, tile* tiles);
+	bool leftSide(GameObject*	gameObject, tile* tiles);
+	bool topSide(GameObject*	gameObject, tile* tiles);
+	bool botSide(GameObject*	gameObject, tile* tiles);
 public:
 	Collision();
 	void setUpCollision(GameObject* player, tile* tiles);
